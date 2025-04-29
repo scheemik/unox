b1683c53b3540ffb3d3fe2bab91f4a9b4efd142d
GreyAudron <mikhail.schee@mail.utoronto.ca>
2025-04-29 17:58:36 -0400

    docs: create docstrings for package functions

--- unox/src/unox/plotting.py
+++ unox/src/unox/plotting.py
@@ -1,7 +1,27 @@
 import matplotlib.pyplot as plt
 
 def plot_lats_lons(lats, lons):
-    """Plot the given latitudes and longitudes."""
+    """Plot the given latitudes and longitudes.
+
+    This will eventually plot the latitudes and longitudes on a map.
+
+    Parameters
+    ----------
+    lats : numpy.ndarray
+        Array of latitude values.
+    lons : numpy.ndarray
+        Array of longitude values.
+    
+    Returns
+    -------
+    fig : matplotlib.figure.Figure
+        The figure object containing the plot.
+    
+    Examples
+    --------
+    >>> lats, lons = load_lats_lons()
+    >>> fig = plot_lats_lons(lats, lons)
+    """
     # Get the shorter of the two list lengths
     min_length = min(len(lats), len(lons))
     print('min_length', min_length)
--- unox/src/unox/unox.py
+++ unox/src/unox/unox.py
@@ -1,16 +1,53 @@
 import numpy as np
 import os
 
-def load_lats_lons():
-    """Load latitude and longitude data from files."""
-    with open('datafiles/lats.npy', 'rb') as f:
+def load_lats_lons(path='../datafiles/'):
+    """Load latitude and longitude data from files.
+
+    Loads arrays of latitude and longitude values that cover 
+    the region of interest.
+
+    Parameters
+    ----------
+    path : str
+        Relative path to the directory containing data files.
+
+    Returns
+    -------
+    lats : numpy.ndarray
+        Array of latitude values.
+    lons : numpy.ndarray
+        Array of longitude values.
+
+    Examples
+    --------
+    >>> lats, lons = load_lats_lons()
+    """
+    with open(path+'/lats.npy', 'rb') as f:
         lats = np.load(f)
-    with open('datafiles/lons.npy', 'rb') as f:
+    with open(path+'/lons.npy', 'rb') as f:
         lons = np.load(f)
     return lats, lons
 
 def show_available_data(path='original_sample_data/'):
-    """Prints a list of available data in the given directory."""
+    """Print a list of available data in the given directory.
+    
+    For the given path, this function will print all the files in the directory.
+
+    Parameters
+    ----------
+    path : str
+        Relative path to the directory containing data files.
+
+    Returns
+    -------
+    data_files : list
+        List of file paths in the given directory.
+
+    Examples
+    --------
+    >>> data_files = show_available_data('original_sample_data/')
+    """
     # Check if the path exists
     if not os.path.exists(path):
         try:
@@ -19,19 +56,44 @@ def show_available_data(path='original_sample_data/'):
         except:
             print(f"Path {path} does not exist.")
             return []
+    # Recursively get all files in the directory
     data_files = recursive_paths(path)
+    # Print the file paths
     for data_file in data_files:
         print(data_file)
     return data_files
 
 def recursive_paths(path):
-    """Recursively lists all files in the given path."""
+    """Create list recursively of all files in the given path.
+
+    Calls itself recursively to get all files in the given path.
+    Assumes the path is a directory that exists, as is confirmed
+    when called from show_available_data()
+
+    Parameters
+    ----------
+    path : str
+        Relative path to the directory containing data files.
+
+    Returns
+    -------
+    path_list : list
+        List of file paths in the given directory.
+
+    Examples
+    --------
+    >>> path_list = recursive_paths('original_sample_data/')
+    """
+    # Create an empty list in which to store the paths
     path_list = []
+    # Iterate through all entries in the directory
     for entry in os.listdir(path):
         full_path = os.path.join(path, entry)
         if os.path.isdir(full_path):
+            # If the entry is a directory, call this function recursively
             for this_path in recursive_paths(full_path):
                 path_list.append(this_path)
         else:
+            # If the entry is a file, add it to the list
             path_list.append(full_path)
     return path_list
