ebcfd4b3cca2199805976a600072c8d475cf2f9b
GreyAudron <mikhail.schee@mail.utoronto.ca>
2025-04-29 12:02:26 -0400

    test: add unit test for show_available_data

--- unox/src/unox/unox.py
+++ unox/src/unox/unox.py
@@ -11,9 +11,27 @@ def load_lats_lons():
 
 def show_available_data(path='original_sample_data/'):
     """Prints a list of available data in the given directory."""
+    # Check if the path exists
+    if not os.path.exists(path):
+        try:
+            path = '../' + path
+            os.path.exists(path) == True
+        except:
+            print(f"Path {path} does not exist.")
+            return []
+    data_files = recursive_paths(path)
+    for data_file in data_files:
+        print(data_file)
+    return data_files
+
+def recursive_paths(path):
+    """Recursively lists all files in the given path."""
+    path_list = []
     for entry in os.listdir(path):
         full_path = os.path.join(path, entry)
         if os.path.isdir(full_path):
-            show_available_data(full_path)
+            for this_path in recursive_paths(full_path):
+                path_list.append(this_path)
         else:
-            print(full_path)
+            path_list.append(full_path)
+    return path_list
--- unox/tests/test_unox.py
+++ unox/tests/test_unox.py
@@ -1,1 +1,10 @@
 from unox import unox
+
+def test_show_available_data():
+    """Test the show_available_data function."""
+    expected = ['../original_sample_data/stage1/x/X_2005.npy', 
+                '../original_sample_data/stage1/y/Y_2005.npy', 
+                '../original_sample_data/stage2/x/X_2014.npy', 
+                '../original_sample_data/stage2/y/Y_2014.npy']
+    actual = unox.show_available_data('original_sample_data/')
+    assert actual == expected, f"Expected file list (length {len(expected)}) does not match actual file list (length {len(actual)})"
