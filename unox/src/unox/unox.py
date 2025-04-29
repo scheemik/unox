cee9e05c6b488f8c24bff22a587d63d28a885ccf
GreyAudron <mikhail.schee@mail.utoronto.ca>
2025-04-29 11:36:30 -0400

    feat: add function to list available data

--- unox/src/unox/unox.py
+++ unox/src/unox/unox.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 
 def load_lats_lons():
     """Load latitude and longitude data from files."""
@@ -7,3 +8,12 @@ def load_lats_lons():
     with open('datafiles/lons.npy', 'rb') as f:
         lons = np.load(f)
     return lats, lons
+
+def show_available_data(path='original_sample_data/'):
+    """Prints a list of available data in the given directory."""
+    for entry in os.listdir(path):
+        full_path = os.path.join(path, entry)
+        if os.path.isdir(full_path):
+            show_available_data(full_path)
+        else:
+            print(full_path)
