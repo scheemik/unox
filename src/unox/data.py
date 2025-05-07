34dfa11a0e97ffec339253332c6ec67dc3e4f49b
GreyAudron <mikhail.schee@mail.utoronto.ca>
2025-05-07 14:32:42 -0400

    feat: add functions to verify coordinates and shift lons

--- src/unox/data.py
+++ src/unox/data.py
@@ -33,30 +33,19 @@ def get_extent(xr_dataset,
     lat_max = np.unique(xr_dataset.lat.max().values)[0]
     lon_min = np.unique(xr_dataset.lon.min().values)[0]
     lon_max = np.unique(xr_dataset.lon.max().values)[0]
-    # Verify that the min and max values are not NaN
-    if np.isnan(lat_min):
-        raise ValueError("lat_min is NaN.")
-    if np.isnan(lat_max):
-        raise ValueError("lat_max is NaN.")
-    if np.isnan(lon_min):
-        raise ValueError("lon_min is NaN.")
-    if np.isnan(lon_max):
-        raise ValueError("lon_max is NaN.")
     # Verify that latitude values are in the range [-90, 90]
-    if lat_min < -90 or lat_min > 90:
-        raise ValueError(f"Latitude values must be in the range [-90, 90], lat_min = {lat_min}.")
-    if lat_max < -90 or lat_max > 90:
-        raise ValueError(f"Latitude values must be in the range [-90, 90], lat_max = {lat_max}.")
+    lat_max = verify_lat(lat_max)
+    lat_min = verify_lat(lat_min)
     # Verify that longitude values are in the range [-180, 180]
     if shift_lons:
-        lon_min -= 180
-        lon_max -= 180
-    if lon_min < -180 or lon_min > 180:
-        raise ValueError(f"Longitude values must be in the range [-180, 180], lon_min = {lon_min}.\n\t If values are in the range [0, 360], set shift_lons=True.")
-    if lon_max < -180 or lon_max > 180:
-        raise ValueError(f"Longitude values must be in the range [-180, 180], lon_max = {lon_max}.\n\t If values are in the range [0, 360], set shift_lons=True.")
+        lon_min = shift_lon(lon_min)
+        lon_max = shift_lon(lon_max)
+    lon_max = verify_lon(lon_max)
+    lon_min = verify_lon(lon_min)
     # Return the extent as a tuple
     return (lat_min, lat_max, lon_min, lon_max)
+
+# def compare_lats_lons((lats1, lons1), (lats2, lons2)):
 
 def verify_dataset(xr_dataset):
     """Verify that the given xarray dataset is valid.
@@ -78,3 +67,131 @@ def verify_dataset(xr_dataset):
     # Verify that the dataset has the time coordinate
     if 'time' not in xr_dataset.coords:
         raise ValueError("xr_dataset must have 'time' coordinate.")
+
+def verify_number(value):
+    """Verify that the given value is a number.
+
+    If the given value is a number that can be converted to
+    an integer but is not a string or character, return True. 
+    Otherwise, return False.
+
+    Parameters
+    ----------
+    value : any
+        The value to verify.
+
+    Returns
+    -------
+    bool
+        True if the value is a number, False otherwise.
+
+    Examples
+    --------
+    >>> value = verify_number(5)
+    True
+    >>> value = verify_number("5")
+    False
+    >>> value = verify_number(np.nan)
+    False
+    """
+    if isinstance(value, str) or isinstance(value, bytes):
+        return False
+    try:
+        foo = int(value)
+        return True
+    except:
+        return False
+
+def verify_lat(lat_val):
+    """Verify that the given latitude value is valid.
+
+    If the given latitude value is within the range [-90, 90],
+    return that value. Otherwise, raise a ValueError.
+
+    Parameters
+    ----------
+    lat_val : float
+        The latitude value to verify.
+
+    Returns
+    -------
+    lat_val : float
+        The verified latitude value.
+
+    Examples
+    --------
+    >>> lat_val = verify_lat(45.0)
+    45.0
+    >>> lat_val = verify_lat(-100.0)
+    ValueError: Latitude value must be in the range [-90, 90].
+    """
+    if not verify_number(lat_val):
+        raise ValueError("Latitude value must be a number.")
+    if np.isnan(lat_val):
+        raise ValueError("Latitude value must not be NaN.")
+    if lat_val < -90 or lat_val > 90:
+        raise ValueError(f"Latitude value must be in the range [-90, 90], lat_val = {lat_val}.")
+    return lat_val
+
+def verify_lon(lon_val):
+    """Verify that the given longitude value is valid.
+
+    If the given longitude value is within the range [-180, 180],
+    return that value. Otherwise, raise a ValueError.
+
+    Parameters
+    ----------
+    lon_val : float
+        The longitude value to verify.
+
+    Returns
+    -------
+    lon_val : float
+        The verified longitude value.
+
+    Examples
+    --------
+    >>> lon_val = verify_lon(45.0)
+    45.0
+    >>> lon_val = verify_lon(-200.0)
+    ValueError: Longitude value must be in the range [-180, 180].
+    """
+    if not verify_number(lon_val):
+        raise ValueError("Longitude value must be a number.")
+    if np.isnan(lon_val):
+        raise ValueError("Longitude value must not be NaN.")
+    if lon_val < -180 or lon_val > 180:
+        raise ValueError(f"Longitude value must be in the range [-180, 180], lon_val = {lon_val}.")
+    return lon_val
+
+def shift_lon(lon_value):
+    """Shift the given longitude value from the range [0, 360] to [-180, 180].
+
+    If the given longitude value is within the range [0, 360],
+    return that value shifted to the range [-180, 180].
+    Otherwise, raise a ValueError.
+
+    Parameters
+    ----------
+    lon_value : float
+        The longitude value to shift.
+
+    Returns
+    -------
+    lon_value : float
+        The shifted longitude value.
+
+    Examples
+    --------
+    >>> lon_value = shift_lon(45.0)
+    -315.0
+    >>> lon_value = shift_lon(-200.0)
+    ValueError: Longitude value must be in the range [0, 360].
+    """
+    if not verify_number(lon_value):
+        raise ValueError("Longitude value must be a number.")
+    if np.isnan(lon_value):
+        raise ValueError("Longitude value must not be NaN.")
+    if lon_value < 0 or lon_value > 360:
+        raise ValueError(f"Longitude value must be in the range [0, 360], lon_value = {lon_value}.")
+    return lon_value - 180
