934bd539edaf7afd17eb9c820551f60804bff2f0
GreyAudron <mikhail.schee@mail.utoronto.ca>
2025-05-09 14:22:33 -0400

    feat: add function to plot maps from .npy files

--- src/unox/plotting.py
+++ src/unox/plotting.py
@@ -98,18 +98,21 @@ def plot_lats_lons(xr_dataset=xr.open_dataset('../datafiles/nox_2019_t106_US.nc'
     # Return the figure
     return fig
 
-def plot_nox(datafile='../datafiles/nox_2019_t106_US.nc',
-             datetime='2019-01-01T00:00:00',
-             cbar_max=1.2e-10,
-             padding=0.1):
-    """Plots a map of NOx data.
+def plot_nc_map(datafile='../datafiles/nox_2019_t106_US.nc',
+                var='nox',
+                datetime='2019-01-01T00:00:00',
+                cbar_max=1.2e-10,
+                padding=0.1):
+    """Plots a map of the 'var' data in a netCDF.
 
-    Creates a map of NOx data on a map using the provided data file.
+    Creates a map of the 'var' data on a map using the provided netCDF file.
 
     Parameters
     ----------
     datafile : str
-        Path to the data file containing NOx data.
+        Path to the netCDF data file.
+    var : str
+        The name of the variable to plot from the netCDF file.
     datetime : str
         Date and time to select from the data file.
     cbar_max : float
@@ -149,6 +152,70 @@ def plot_nox(datafile='../datafiles/nox_2019_t106_US.nc',
     # Add a colorbar
     fig.colorbar(this_nox, loc='b', label='NOx emissions (kg/m2/s)')
     # Return the figure
+    return fig
+
+def plot_npy_map(truth_params={'stage': 1, 'x_or_y': 'y', 'year': 2019},
+                pred_params={'stage': -1, 'HPC_run': 'test_unet_601760', 'year': 2019},
+                var='nox',
+                datetime='2019-01-01T00:00:00',
+                cbar_max=1.2e-10,
+                padding=0.1):
+    """Plots a map of data in a npy file.
+
+    Creates a map of the 'var' data on a map using the provided netCDF file.
+
+    Parameters
+    ----------
+    datafile : str
+        Path to the netCDF data file.
+    var : str
+        The name of the variable to plot from the netCDF file.
+    datetime : str
+        Date and time to select from the data file.
+    cbar_max : float
+        Maximum value for the colorbar.
+    
+    Returns
+    -------
+    """
+    truth = np.load(unox.get_sample_data(**truth_params))
+    # Remove `stage` from pred_params, if present
+    pred_params.pop('stage', None)
+    stage1 = np.load(unox.get_pred_data(stage=1, **pred_params))
+    stage2 = np.load(unox.get_pred_data(stage=2, **pred_params))
+
+    lats, lons = unox.load_lats_lons()
+
+    fig, ax = plt.subplots(2,3,figsize=(14,8))
+    vmin = np.min(np.concatenate([truth,stage1,stage2]))
+    vmax = np.max(np.concatenate([truth,stage1,stage2]))
+    halfrange = np.max(np.abs([vmin,vmax]))/2
+
+    day = 200  #day of the year to look at
+    plt.sca(ax[0,0])
+    plt.pcolormesh(lons,lats,truth[day,:,:,0],cmap=plt.cm.seismic,norm=mpl.colors.CenteredNorm(halfrange=halfrange))  
+    plt.title('NOx emissions (truth)')
+    plt.colorbar()
+    plt.sca(ax[0,1])
+    plt.pcolormesh(lons,lats,truth[day,:,:,0]-stage1[day,:,:,0],norm=mpl.colors.CenteredNorm(halfrange=halfrange),cmap=plt.cm.seismic)  
+    plt.title('Truth - stage 1 prediction')
+    plt.colorbar()
+    plt.sca(ax[0,2])
+    plt.pcolormesh(lons,lats,truth[day,:,:,0]-stage2[day,:,:,0],norm=mpl.colors.CenteredNorm(halfrange=halfrange),cmap=plt.cm.seismic)  
+    plt.title('Truth - stage 2 prediction')
+    plt.colorbar()
+    plt.sca(ax[1,0])
+    plt.pcolormesh(lons,lats,stage1[day,:,:,0],cmap=plt.cm.seismic,norm=mpl.colors.CenteredNorm(halfrange=halfrange))  
+    plt.title('Stage 1 prediction')
+    plt.colorbar()
+    plt.sca(ax[1,1])
+    plt.pcolormesh(lons,lats,stage2[day,:,:,0],cmap=plt.cm.seismic,norm=mpl.colors.CenteredNorm(halfrange=halfrange))  
+    plt.title('Stage 2 prediction')
+    plt.colorbar()
+    plt.sca(ax[1,2])
+    plt.pcolormesh(lons,lats,stage1[day,:,:,0]-stage2[day,:,:,0],norm=mpl.colors.CenteredNorm(halfrange=halfrange),cmap=plt.cm.seismic)  
+    plt.title('Stage 1 - stage 2 prediction')
+    plt.colorbar()
     return fig
 
 def plot_comparison(truth_data={'stage':1, 'x_or_y':'y', 'year':2019},
