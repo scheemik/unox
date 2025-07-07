b5d508e2dcc09e32fd4c89160565871535d78067
Mikhail Schee <mikhail.schee@alumni.utoronto.ca>
2025-07-07 14:11:44 -0400

    test: add unit test for making x input file

--- tests/test_input.py
+++ tests/test_input.py
@@ -16,13 +16,13 @@ def test_make_y_input_file(datadir='/data/high_res/emacdonald/unet/datafiles/t10
     # Call the function to create the y input file
     y_data = uin.make_y_input_file(year=this_year, 
                                    var='nox',
-                                   datadir=datadir,
-                                   fileprefix='nox_',
-                                   fileextension='_t106_US.nc',
+                                   emiss_dir=datadir,
+                                   emiss_pre='nox_',
+                                   emiss_post='_t106_US.nc',
                                    scale_factor=1e12,
                                    nan_fill=0,
                                    stage_2_cutoff=2022,
-                                   outputdir=None
+                                   output_dir=None
                                    )
     # Verify that the output is a numpy array
     assert isinstance(y_data, np.ndarray), "make_y_input_file did not return a numpy array."
@@ -30,3 +30,37 @@ def test_make_y_input_file(datadir='/data/high_res/emacdonald/unet/datafiles/t10
     assert y_data.shape == verify_array.shape, f"make_y_input_file output shape {y_data.shape} does not match verification array shape {verify_array.shape}"
     # Verify that the output matches the verification array
     assert np.array_equal(y_data, verify_array), f"make_y_input_file output does not match array from {verify_filepath}"
+
+def test_make_x_input_file(datadir='/data/high_res/emacdonald/unet/datafiles/', 
+                           verifydir='inputfiles/',
+                           this_year=2019
+                           ):
+    """Test the make_x_input_file function."""
+    for this_stage in [1, 2]:
+        # Assemble file path to verification array
+        verify_filepath = f"{verifydir}stage{this_stage}/x/X_{this_year}.npy"
+        # Verify that file path
+        verify_filepath = unox.verify_path(verify_filepath)
+        # Load the verification array
+        verify_array = np.load(verify_filepath)
+        # Call the function to create the x input file
+        x_data = uin.make_x_input_file(
+            year=this_year, 
+            stage=this_stage,
+            data_dir=datadir,
+            chemra_path='TROPESS/TROPESS_reanalysis_2hr_no2_sfc_',
+            insitu_path='US_EPA/daily_42602_',
+            era5_path='ERA5concatenated/',
+            scale_factors={
+                'chemra': 1000,
+                'sp': 100000,
+                'ssrd': 1000000,
+                'blh': 1000},
+            output_dir=None
+        )
+        # Verify that the output is a numpy array
+        assert isinstance(x_data, np.ndarray), "make_x_input_file did not return a numpy array."
+        # Verify that the shape of the output matches the verification array
+        assert x_data.shape == verify_array.shape, f"make_x_input_file output shape {x_data.shape} does not match verification array shape {verify_array.shape}"
+        # Verify that the output matches the verification array
+        assert np.array_equal(x_data, verify_array), f"make_x_input_file output does not match array from {verify_filepath}"
