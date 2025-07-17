bff94d690bb4cbda36a015e6617c15a6b01f8151
Mikhail Schee <mikhail.schee@alumni.utoronto.ca>
2025-07-17 15:20:16 -0400

    feat: add function to recursively delete everything in a non-empty directory

--- src/unox/unox.py
+++ src/unox/unox.py
@@ -73,6 +73,32 @@ def verify_path(path):
     else:
         return path
 
+def remove_non_empty_directory(base_dir):
+    """Remove a non-empty directory and all its contents.
+
+    This function will recursively delete all files and directories in the given path.
+
+    Parameters
+    ----------
+    base_dir : str
+        Relative path to the directory to be removed.
+    """
+    # Verify the path
+    top = verify_path(base_dir)
+    # Check if the path is a directory
+    if not os.path.isdir(top):
+        raise ValueError(f"Path {top} is not a directory.")
+    # Recursively remove all files and directories in the given path
+    for root, dirs, files in os.walk(top, topdown=False):
+        for name in files:
+            print(f'Removing file: {os.path.join(root, name)}')
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+            print(f'Removing directory: {os.path.join(root, name)}')
+            os.rmdir(os.path.join(root, name))
+    # Finally remove the top directory itself
+    os.rmdir(top)  
+
 def show_available_data(path='original_sample_data/', verb=False):
     """Print a list of available data in the given directory.
     
