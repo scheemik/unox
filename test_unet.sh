0b72b078d92d1740e48744e1d3252e24ff96c7ce
Mikhail Schee <mschee@physics.utoronto.ca>
2025-08-22 15:39:38 -0400

    feat: add argument to call a version of the code to run

--- test_unet.sh
+++ test_unet.sh
@@ -6,18 +6,26 @@
 #SBATCH --mail-type=ALL
 #SBATCH --output=HPC_runs/%x/log_%x_%j.txt				# %x = job_name, %j = job_number
 
-# Submit this script to a HPC with `sbatch`
+# Submit this script to a HPC with `sbatch`. Note, by default the code will run 
+# with updated versions of tensorflow and keras, which won't work on Mist. Use
+# `-v 0` to run with the versions that are compatible with Mist.
 # Takes in optional arguments:
 #	$ sbatch test_unet.sh -j <job name> 				 Default: test_unet
+#                         -v <version>                   Default: 1, use updates
+#                         -c <cluster>                   Default: trillium
 
 # Having a ":" after a flag means an option is required to invoke that flag
-while getopts j: option
+while getopts j:v:c: option
 do
 	case "${option}"
 		in
 		j) JOBNAME=${OPTARG};;
+		v) VERSION=${OPTARG};;
+		c) CLUSTER=${OPTARG};;
 	esac
 done
+
+CODEFILE='test_unet.py'
 
 # check to see if arguments were passed
 if [ -z "$JOBNAME" ]
@@ -27,11 +35,72 @@ then
 else
 	echo "-j, Name specified, using JOBNAME=$JOBNAME"
 fi
+if [ -z "$VERSION" ]
+then
+	VERSION=1
+	echo "-v, No version specified, using VERSION=$VERSION"
+else
+	echo "-v, Version specified, using VERSION=$VERSION"
+fi
+if [ -z "$CLUSTER" ]
+then
+    CLUSTER="trillium"
+    echo "-c, No cluster specified, defaulting to $CLUSTER"
+else
+    echo "-c, Using cluster: $CLUSTER"
+fi
 
-module load MistEnv/2021a
-module load anaconda3/2021.05 
-source activate unetmist
-module load cuda/11.4.4 
+# Load modules and activate virtual environment
+if [ "$CLUSTER" = "trillium" ]
+then
+	echo "Loading modules for Trillium HPC environment"
+	echo ""
+	if [ "$VERSION" = 0 ]
+	then
+		echo "-v $VERSION, using original code"
+		module load StdEnv/2020 gcc/9.3.0 python/3.8.10 cuda/11.4
+		ENVDIR="/home/mschee/.virtualenvs/unoxTrillium"
+	elif [ "$VERSION" = 1 ]
+	then
+		echo "-v $VERSION, using updated code"
+		module load StdEnv/2023 gcc/12.3 python/3.12.4 cuda/12.6
+		ENVDIR="/home/mschee/.virtualenvs/unoxTrilliumNew"
+	else
+		echo "Version $VERSION not recognized, exiting"
+		exit 1
+	fi
+	echo ""
+	echo "Activating virtualenv from $ENVDIR/bin/activate"
+	source $ENVDIR/bin/activate
+elif [ "$CLUSTER" = "mist" ]
+then
+	echo "Loading modules for Mist HPC environment"
+	echo ""
+	if [ "$VERSION" = 0 ]
+	then
+		echo "-v $VERSION, using original code"
+		module load MistEnv/2021a anaconda3/2021.05 
+		source activate unetmist
+		module load cuda/11.4.4
+	else
+		echo "Version $VERSION not recognized, exiting"
+		exit 1
+	fi
+else
+	echo "Cluster $CLUSTER not recognized, exiting"
+	exit 1
+fi
+
+# source /home/mschee/.virtualenvs/unoxTrillium/bin/activate
+# source /home/mschee/.virtualenvs/unoxTrilliumNew/bin/activate
+
+# Mist module environment
+# module load MistEnv/2021a
+# module load anaconda3/2021.05 
+# source activate unetmist
+# module load cuda/11.4.4 
+
+# Ignore these
 # module load anaconda3/2021.05 cuda/11.4.4 gcc/10.3.0 openblas/0.3.15 openmpi/4.1.1+ucx-1.10.0 hdf5/1.10.7
 
 SAVEDIR="HPC_runs/${JOBNAME}" #_${SLURM_JOB_ID}"
@@ -43,5 +112,8 @@ then
 else
 	echo "Directory for job $SAVEDIR already exists"
 fi
- 
-python test_unet.py $SAVEDIR
+
+echo ""
+echo "Running $CODEFILE with savedir $SAVEDIR"
+echo ""
+python $CODEFILE $SAVEDIR $VERSION
