29d4506bf7b96a3173437e183ca7d606819ff5d9
Mikhail Schee <mikhail.schee@alumni.utoronto.ca>
2025-09-08 17:21:08 -0400

    style: generalize comments to any HPC, not just Mist

--- HPC_to_animus.sh
+++ HPC_to_animus.sh
@@ -2,14 +2,15 @@
 # Author: Mikhail Schee
 # 2025-06-02
 
-# Run this script to copy a file or directory from Mist to Animus.
+# Run this script to copy a file or directory from an HPC cluster to Animus.
+# To be run on Animus.
 # Takes in the following arguments:
-#	$ bash HPC_mist_to_animus.sh -f <filename>      Ex: test_unet_601760
-#                                -j <HPC_job>       Look for HPC job based on filename
-#                                -c <cluster>       HPC cluster to transfer from (default: trillium)
+#	$ bash HPC_to_animus.sh -f <filename>      Ex: test_unet_601760
+#                           -j <HPC_job>       Look for HPC job based on filename
+#                           -c <cluster>       HPC cluster to transfer from (default: trillium)
 #
 # Note: Each file in list must be preceded by the -f flag. Ex:
-#	$ bash HPC_mist_to_animus.sh -f test_file1 -f test_file2
+#	$ bash HPC_to_animus.sh -f test_file1 -f test_file2
 
 # Having a ":" after a flag means an option is required to invoke that flag
 while getopts "f:jc:" option;
@@ -39,11 +40,9 @@ fi
 if [ "$HPC_JOB" = j ]
 then
 	DIR_PREFIX="/HPC_runs"
-    EXCLUDE_FLAG="!(.h5)"
 	echo "-j, Copying full HPC job directory for ${FILENAMES[*]} from $CLUSTER to Animus"
 else
 	DIR_PREFIX=""
-    EXCLUDE_FLAG=""
 fi
 
 ###############################################################################
@@ -70,7 +69,7 @@ IDENTITY_FILE="~/.ssh/id_ed25519"
 # If copying a job, only copy the contents of `stage1_output` and 
 # `stage2_output`. Also copy the `.txt` file with the same name
 if [ "$HPC_JOB" = j ]; then
-    # Copy job directories from Mist to Animus
+    # Copy job directories from HPC to Animus
     for FILE in "${FILENAMES[@]}"; do
         FILES=""
         # Check whether the corresponding directory exists on Animus
@@ -86,7 +85,7 @@ if [ "$HPC_JOB" = j ]; then
         scp -r -i $IDENTITY_FILE $USERNAME@$REMOTE_SERVER:"$FILES" .$DIR_PREFIX/$FILE
     done
 else
-    # Copy files or directories from Mist to Animus
+    # Copy files or directories from HPC to Animus
     FILES=""
     for FILE in "${FILENAMES[@]}"; do
         FILES+="$PROJECT_DIR$DIR_PREFIX/$FILE "
