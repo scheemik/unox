bfa3f7c6b5bf61694cecfeaa1d5637e2ce7981af
Mikhail Schee <mikhail.schee@alumni.utoronto.ca>
2025-10-01 10:34:17 -0400

    feat: add function to load trained model from file

---
+++ src/unox/model.py
@@ -0,0 +1,34 @@
+from tensorflow.keras import models
+
+from unox import unox
+# Import functions used in building models to allow for importing custom objects
+from unox.HPC_scripts.utils import functions
+
+def load_model(
+    model_path: str,
+    ):
+    """Load a trained model from a file.
+
+    Parameters
+    ----------
+    model_path : str
+        Path to the model file. Should be an .h5 or .keras file.
+
+    Returns
+    -------
+    model : tf.keras.Model
+        The loaded model.
+
+    Examples
+    --------
+    >>> model = load_model('model.h5')
+    >>> model.summary()
+    """
+    # Verify the model path
+    model_path = unox.verify_path(model_path)
+    # Vertify the path ends with .h5 or .keras
+    if not (model_path.endswith('.h5') or model_path.endswith('.keras')):
+        raise ValueError("Model path must end with .h5 or .keras")
+    # Load the model
+    model = models.load_model(model_path)
+    return model
