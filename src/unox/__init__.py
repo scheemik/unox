bbf7ff7c0db164002a1f10a3dee88ab89cd07eee
Mikhail Schee <mikhail.schee@alumni.utoronto.ca>
2025-10-01 10:32:42 -0400

    feat: add check for tensorflow version number

--- src/unox/__init__.py
+++ src/unox/__init__.py
@@ -1,3 +1,9 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("unox")
+
+# Verify version of tensorflow package
+import tensorflow as tf
+target_version = "2.17.0"
+if tf.__version__ < target_version:
+    raise ImportError(f"TensorFlow version must be {target_version} or higher, got: {tf.__version__}")
