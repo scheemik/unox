46a410f70fa473b44482af9d05413a94bbfbf364
Mikhail Schee <mikhail.schee@alumni.utoronto.ca>
2025-11-06 15:21:04 -0500

    style: modify comments to fully describe the dimensions of the objects at every step

--- src/unox/HPC_scripts/model/core_tl.py
+++ src/unox/HPC_scripts/model/core_tl.py
@@ -9,32 +9,35 @@ import tensorflow as tf
 
 @keras.saving.register_keras_serializable(package="unox", name="build_Unet")
 def build_Unet():
-    inputs = Input( ( 35, 46, 9 ), name='model_input')
+    inputs = Input( ( 35, 46, 9 ), name='model_input') # (None, 35, 46, 9)
 
-    c1 = Conv2D(128, (3, 3), activation='softplus', padding='same', name='Block1_Conv1') (inputs)    # 35, 46
-    c1 = Conv2D(256, (3, 3), activation='softplus', padding='same', name='Block1_Conv2') (c1)   # 35, 46
-    p1 = MaxPooling2D((2, 2), name='Block1_MaxPool', padding='same') (c1)   # 18 23
+    # Conv2D(filters, kernel_size, **kwargs)
+    ## filters is the dimension of the output space, the number of filters in the convolution
+    c1 = Conv2D(128, (3, 3), activation='softplus', padding='same', name='Block1_Conv1') (inputs) # (None, 35, 46, 128)
+    c1 = Conv2D(256, (3, 3), activation='softplus', padding='same', name='Block1_Conv2') (c1)   # (None, 35, 46, 256)
+    p1 = MaxPooling2D((2, 2), name='Block1_MaxPool', padding='same') (c1)   # (None, 18, 23, 256)
 
-    c2 = Conv2D(256, (3, 3), activation='softplus', padding='same', name='Block2_Conv1') (p1)   # 18 23
-    c2 = Conv2D(512, (3, 3), activation='softplus', padding='same', name='Block2_Conv2') (c2)   # 18 23
-    p2 = MaxPooling2D((2, 2), name='Block2_MaxPool', padding='same') (c2)   # 9 12
+    c2 = Conv2D(256, (3, 3), activation='softplus', padding='same', name='Block2_Conv1') (p1)   # (None, 18, 23, 256)
+    c2 = Conv2D(512, (3, 3), activation='softplus', padding='same', name='Block2_Conv2') (c2)   # (None, 18, 23, 512)
+    p2 = MaxPooling2D((2, 2), name='Block2_MaxPool', padding='same') (c2)   # (None, 9, 12, 512)
 
-    c3 = Conv2D(512, (3, 3), activation='softplus', padding='same', name='Block3_Conv1') (p2)   # 9 12
-    c3 = Conv2D(1024, (3, 3), activation='softplus', padding='same', name='Block3_Conv2') (c3)   # 9 12
-    p3 = MaxPooling2D((2, 2), name='Block3_MaxPool', padding='same') (c3)  # 5 x 6
+    c3 = Conv2D(512, (3, 3), activation='softplus', padding='same', name='Block3_Conv1') (p2)   # (None, 9, 12, 512)
+    c3 = Conv2D(1024, (3, 3), activation='softplus', padding='same', name='Block3_Conv2') (c3)   # (None, 9, 12, 1024)
+    p3 = MaxPooling2D((2, 2), name='Block3_MaxPool', padding='same') (c3)  # (None, 5, 6, 1024)
 
 
-    c4 = Conv2D(1024, (3, 3), activation='softplus', padding='same', name='Block4_Conv1') (p3) # 5 x 6
-    c4 = Conv2D(1024, (3, 3), activation='softplus', padding='same', name='Block4_Conv2') (c4) # 5 x 6
+    c4 = Conv2D(1024, (3, 3), activation='softplus', padding='same', name='Block4_Conv1') (p3) # (None, 5, 6, 1024)
+    c4 = Conv2D(1024, (3, 3), activation='softplus', padding='same', name='Block4_Conv2') (c4) # (None, 5, 6, 1024)
 
-    c4 = Permute((3, 1, 2), name='Block4_Permute1') (c4)
-    c4 = Reshape((-1, 30), name='Block4_Reshape') (c4)
-    f4 = Permute((2, 1), name='Block4_Permute2') (c4)  # 20 x 512
+    c4 = Permute((3, 1, 2), name='Block4_Permute1') (c4) # (None, 1024, 5, 6)
+    c4 = Reshape((-1, 30), name='Block4_Reshape') (c4) # (None, 1024, 30)
+    f4 = Permute((2, 1), name='Block4_Permute2') (c4)  # (None, 30, 1024)
 
-    lstm = LSTM(1024, return_sequences=True, name='LSTM1') (f4)
-    lstm = LSTM(1024, return_sequences=True, name='LSTM2') (lstm)
+    # RNN - Recurrent Neural Networks
+    lstm = LSTM(1024, return_sequences=True, name='LSTM1') (f4) # (None, 30, 1024)
+    lstm = LSTM(1024, return_sequences=True, name='LSTM2') (lstm) # (None, 30, 1024)
 
-    resh = Reshape( (5 , 6, 1024) , name='Block5_Reshape') (lstm)
+    resh = Reshape( (5 , 6, 1024) , name='Block5_Reshape') (lstm) # (None, 5, 6, 1024)
 
     u5 = Conv2DTranspose(512, (2, 2), strides=(2, 2), padding='same', name='Block5_UpConv') (resh) # (None, 10, 12, 512)
     # tf.slice(input_, begin, size)
@@ -46,20 +49,20 @@ def build_Unet():
     c5 = Conv2D(256, (3, 3), activation='softplus', padding='same', name='Block5_Conv1') (u5_comb)  # (None, 9, 12, 256)
     c5 = Conv2D(256, (3, 3), activation='softplus', padding='same', name='Block5_Conv2') (c5)  # (None, 9, 12, 256)
 
-    u6 = Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same', name='Block6_UpConv') (c5)  # 18 x 24
-    u6_cropped = Lambda(lambda x: tf.slice(x, [0, 0, 0, 0], [-1, 18, 23, -1]))(u6)
-    c1_cropped = Lambda(lambda x: tf.slice(x, [0, 8, 11, 0], [-1, 18, 23, -1]))(c1)
-    u6_comb = concatenate([u6_cropped, c2, c1_cropped])
-    c6 = Conv2D(128, (3, 3), activation='softplus', padding='same', name='Block6_Conv1') (u6_comb)  # 18 x 24
-    c6 = Conv2D(128, (3, 3), activation='softplus', padding='same', name='Block6_Conv2') (c6)  # 18 x 24
+    u6 = Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same', name='Block6_UpConv') (c5)  # (None, 18, 24, 256)
+    u6_cropped = Lambda(lambda x: tf.slice(x, [0, 0, 0, 0], [-1, 18, 23, -1]))(u6) # (None, 18, 23, 256)
+    c1_cropped = Lambda(lambda x: tf.slice(x, [0, 8, 11, 0], [-1, 18, 23, -1]))(c1) # (None, 18, 23, 256)
+    u6_comb = concatenate([u6_cropped, c2, c1_cropped]) # (None, 18, 23, 1024)
+    c6 = Conv2D(128, (3, 3), activation='softplus', padding='same', name='Block6_Conv1') (u6_comb) # (None, 18, 23, 128)
+    c6 = Conv2D(128, (3, 3), activation='softplus', padding='same', name='Block6_Conv2') (c6) # (None, 18, 23, 128)
 
-    u7 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same', name='Block7_UpConv') (c6)  # 36, 46
-    u7_cropped = Lambda(lambda x: tf.slice(x, [0, 0, 0, 0], [-1, 35, 46, -1]))(u7)  # 35, 46
-    u7_comb = concatenate([u7_cropped, c1])
-    c7 = Conv2D(64, (3, 3), activation='softplus', padding='same', name='Block7_Conv1') (u7_comb)  # 35, 46
-    c7 = Conv2D(64, (3, 3), activation='softplus', padding='same', name='Block7_Conv2') (c7)  # 35, 46
+    u7 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same', name='Block7_UpConv') (c6) # (None, 36, 46, 128)
+    u7_cropped = Lambda(lambda x: tf.slice(x, [0, 0, 0, 0], [-1, 35, 46, -1]))(u7) # (None, 35, 46, 128)
+    u7_comb = concatenate([u7_cropped, c1]) # (None, 35, 46, 384)
+    c7 = Conv2D(64, (3, 3), activation='softplus', padding='same', name='Block7_Conv1') (u7_comb) # (None, 35, 46, 64)
+    c7 = Conv2D(64, (3, 3), activation='softplus', padding='same', name='Block7_Conv2') (c7) # (None, 35, 46, 64)
 
-    outputs = Conv2D(1, (1, 1), activation='softplus', name='model_output') (c7)
+    outputs = Conv2D(1, (1, 1), activation='softplus', name='model_output') (c7) # (None, 35, 46, 1)
 
     # prepare model here
     model = Model(inputs=[inputs], outputs=[outputs])
