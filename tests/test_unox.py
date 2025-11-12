d051ccf8108493e13ad8d781c57fdd129fea4928
Mikhail Schee <mikhail.schee@alumni.utoronto.ca>
2025-11-12 17:50:41 -0500

    refactor: move test_verify_path to separate module

--- tests/test_unox.py
+++ tests/test_unox.py
@@ -2,30 +2,6 @@ from unox import unox
 import os
 import numpy as np
 
-def test_verify_path():
-    """Test the verify_path function."""
-    # Test with valid path
-    valid_path = 'inputfiles/no2_sample_input/stage1/x/X_2005.npy'
-    actual = unox.verify_path(valid_path)
-    print(f"Actual path: {actual}")
-    assert type(unox.verify_path(valid_path)) == type('str'), f"verify_path failed on valid path: {valid_path}"
-    # Test with invalid path
-    invalid_path = 'invalid/path/to/file.npy'
-    try:
-        unox.verify_path(invalid_path)
-    except (FileNotFoundError) as e:
-        assert True, f"verify_path raised an exception on invalid path: {e}"
-    else:
-        assert False, f"verify_path did not raise an exception on invalid path {invalid_path}"
-    # Test with invalid path
-    invalid_path = 12345
-    try:
-        unox.verify_path(invalid_path)
-    except (TypeError) as e:
-        assert True, f"verify_path raised an exception on invalid path: {e}"
-    else:
-        assert False, f"verify_path did not raise an exception on invalid path {invalid_path}"
-
 def test_make_file_path():
     """Test the make_file_path function."""
     # Test with valid input
@@ -196,15 +172,15 @@ def test_get_one_t_input_var_array():
 def test_get_pred_data():
     """Test the get_pred_data function."""
     # Test with valid parameters
-    params = {'stage': 1, 'HPC_run': 'test_unet_601760', 'year': 2019}
+    params = {'stage': 1, 'HPC_run': 'no2_example_run', 'year': 2019}
     actual = unox.get_pred_data(**params)
-    expected = 'HPC_runs/test_unet_601760/stage1_output/pred_X_2019.npy'
+    expected = 'HPC_runs/no2_example_run/stage1_output/pred_X_2019.npy'
     assert actual == expected, f"Expected {expected}, but got {actual}"
     
     # Test with invalid parameters
-    invalid_params = [{'stage': 3, 'HPC_run': 'test_unet_601760', 'year': 2019},
+    invalid_params = [{'stage': 3, 'HPC_run': 'no2_example_run', 'year': 2019},
                       {'stage': 1, 'HPC_run': '', 'year': 2019},
-                      {'stage': 1, 'HPC_run': 'test_unet_601760', 'year': -1}]
+                      {'stage': 1, 'HPC_run': 'no2_example_run', 'year': -1}]
     for params in invalid_params:
         try:
             unox.get_pred_data(**params)
