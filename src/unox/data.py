56dc8af707e340459d747c852e884e0a10c5bf41
Mikhail Schee <mikhail.schee@alumni.utoronto.ca>
2025-11-13 14:38:03 -0500

    fix: avoid circular import

--- src/unox/data.py
+++ src/unox/data.py
@@ -6,8 +6,7 @@ import os
 import re
 from datetime import datetime
 
-from unox import unox
-from unox import verify as vfy
+import verify as vfy
 
 # Define the default latitude and longitude extents for this project
 DEFAULT_LAT_MIN = 11
