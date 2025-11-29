ae0da2cb587a0d69cf3036180d11792dfae1b9a4
Mikhail Schee <mikhail.schee@alumni.utoronto.ca>
2025-11-28 19:27:50 -0500

    feat: add function to get a configuration file

---
+++ src/unox/HPC/data0/config.py
@@ -0,0 +1,37 @@
+import json
+
+from .verify_path import verify_path
+
+def get_config(config_file):
+    """Load the configuration from a JSON file.
+
+    Parameters
+    ----------
+    config_file : str or dict
+        The name or path to the configuration JSON file, 
+        or a dictionary in the format of a configuration file.
+
+    Returns
+    -------
+    config_dict : dict
+        The configuration as a dictionary.
+    """
+    # Verify argument types
+    if isinstance(config_file, dict):
+        return config_file
+    elif isinstance(config_file, str):
+        # Check whether that path exists
+        try:
+            config_path = verify_path(config_file)
+        except:
+            config_path = f"inputfiles/_input_configs/{config_file}.json"
+            try:
+                config_path = verify_path(config_path)
+            except:
+                raise FileNotFoundError(f"(get_config) No file found at {config_file} or {config_path}.")
+        # Load the config file
+        with open(f"{config_path}", 'r') as file:
+            config_dict = json.load(file)
+            return config_dict
+    else:
+        raise TypeError(f"(get_config) `config_file` must be a string or dictionary. Got {type(config_file)}.")
