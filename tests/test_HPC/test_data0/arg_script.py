b3537678015e43531d493eac16f5936480deca97
Mikhail Schee <mikhail.schee@alumni.utoronto.ca>
2025-12-03 14:47:00 -0500

    refactor: use the system argument vector directly instead of building list

--- tests/test_HPC/test_data0/arg_script.py
+++ tests/test_HPC/test_data0/arg_script.py
@@ -5,11 +5,8 @@ import unox.HPC.data0.run_functions as rf
 # Argument 0 is always the name of the script
 print(f"Script name (sys.argv[0]): {sys.argv[0]}")
 
-# Make a blank list in which to collect command line arguments
-cmd_args = []
-for arg in sys.argv:
-    cmd_args.append(arg)
+# Print the given command line arguments
+print(f"Command line arguments: {sys.argv}")
 
-print(f"Command line arguments: {cmd_args}")
-
-cmd_args = rf.process_cmd_args(cmd_args)
+# Process the command line arguments
+cmd_args = rf.process_cmd_args(sys.argv)
