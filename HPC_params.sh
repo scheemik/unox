cbc61d0cb81394da6b897311fae2354607773be1
Mikhail Schee <mschee@physics.utoronto.ca>
2026-01-13 15:56:33 -0500

    feat: add check for email in `HPC_slurm.sh`

--- HPC_params.sh
+++ HPC_params.sh
@@ -5,7 +5,15 @@
 # Edit this script so the parameters reference your own setup
 
 # The email to use for sending HPC job notifications
-HPC_EMAIL="mikhail.schee@alumni.utoronto.ca"
+# Read the `HPC_slurm.sh` script to find the email address
+# line-by-line until you find `#SBATCH --mail-user`
+SCRIPT="HPC_slurm.sh"
+while read -r line; do
+    if [[ $line == \#SBATCH\ --mail-user=* ]]; then
+        HPC_EMAIL="${line#*=}"
+        break
+    fi
+done < "$SCRIPT"
 
 # The username on the HPC cluster
 HPC_USERNAME="mschee"
