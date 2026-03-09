01c95fd7016d1bbae7c274208285c527fe358f7b
Mikhail Schee <mschee@physics.utoronto.ca>
2026-03-09 16:09:58 -0400

    refactor(getopts): handle copy of config file to run dir in `HPC_job_submit.sh`, remove `-i` arg in other scripts

--- HPC_GPU_slurm.sh
+++ HPC_GPU_slurm.sh
@@ -12,22 +12,20 @@
 # `-v 0` to run with the versions that are compatible with Mist.
 # Takes in optional arguments:
 #  $ sbatch HPC_GPU_slurm.sh -j <job name> 			Default: test_unet
-#							 -i <config file>       Default: test_config
 #							 -t <run type>			Default: test, other options: zfi_set
 #                            -v <version>           Default: 1, use updates
 #                            -c <cluster>           Default: trillium
 
 # Having a ":" after a flag means an option is required to invoke that flag
-while getopts j:i:t:v:c: option
+while getopts j:t:v:c: option
 do
 	case "${option}"
 		in
 		j) JOBNAME=${OPTARG};;
-		i) CONFIG_FILE=${OPTARG};;
 		t) TYPE=${OPTARG};;
 		v) VERSION=${OPTARG};;
 		c) CLUSTER=${OPTARG};;
 	esac
 done
 
-bash HPC_slurm.sh -j $JOBNAME -i $CONFIG_FILE -t $TYPE -v $VERSION -c $CLUSTER
+bash HPC_slurm.sh -j $JOBNAME -t $TYPE -v $VERSION -c $CLUSTER
